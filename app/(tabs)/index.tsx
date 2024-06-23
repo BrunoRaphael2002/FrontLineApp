86c3acd4f3be571d3b2ea2981fcac31f27e4f071
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2024-06-22 22:27:55 -0300

    reset

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">olá mundo</ThemedText>
+        <ThemedText type="title">olá mundo!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
